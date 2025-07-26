db989db61f8cace4663746a5ffff9c2495af2622
Sara Brentini <sara.brentini@hotmail.com>
2025-07-26 11:04:38 +0300

    Initial commit

---
+++ src/test/java/exercise/eventshuffle/EventshuffleApplicationTests.java
@@ -0,0 +1,13 @@
+package exercise.eventshuffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventshuffleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
